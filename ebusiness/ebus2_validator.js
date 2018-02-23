5602c64c761390f18f30d696434e6d36dd5d17fb
Tom O'Donnell <117367303@umail.ucc.ie>
2018-02-23 13:29:56 +0000

    Ebus page 2 complete

--- ebusiness/ebus2_validator.js
+++ ebusiness/ebus2_validator.js
@@ -2,26 +2,67 @@
 
 function validateDetails(){
     
-    var pin;
+var pin;
     
     pin = document.getElementById("user_pin").value;
     
     if (pin == ""){
-        alert("Please enter your PIN");
+        alert("Please enter your PIN.");
     }
-    else if (String(pin).length < 4){
-        alert("Please make sure your PIN is accurate");
+    else if (pin.length < 4){
+        alert("Your PIN must be 4 digits.");
+    }
+    else{
+        validateName();
+    }
+}
+
+function validateName(){
+    
+    var name;
+    
+    name = document.getElementById("user_name").value;
+
+    if (name == ""){
+        alert("Please enter a name.");
+    }
+    else if (!name.match(/^[a-zA-Z]+$/)){
+        alert('Only letters are allowed in a name.');
+    }
+    else{
+    validateEmail();
+    }
+
+}
+
+function validateEmail(){
+    
+    var email;
+    
+    email = document.getElementById("user_email").value;
+    
+    if (email == ""){
+        alert("please enter an email.");
+    }
+    else if (validateInputEmail(email) == false){
+        alert("Please enter a valid Email ID");
     }
     else{
         enablebtnPurchase();
     }
+}
+
+function validateInputEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+    }
     
-}
-
 function enablebtnPurchase(){
+    
     $('#btnPurchase').prop('disabled', false);
-}
-
+    }
+    
 function disablebtnPurchase() {
+    
     $('#btnPurchase').prop('disabled', true);
-}
+    }
